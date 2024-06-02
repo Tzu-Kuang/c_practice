f2332f5e83d2921d5c03aac581f64ea86b6542c8
Tzu-Kuang <s125097336@gmail.com>
2024-06-02 23:04:12 +0800

    Initial commit

---
+++ Ex_Files_C_Data_Structures_Pointers/Exercise Files/Ch03/answer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*Create a Pointer Array*/
+/*Step 1 : create an array of 10 char pointers*/
+/*Step 2 : prompt the user for string input*/
+/*Step 3 : assign each string input to an address stored  in the array*/
+/*Step 4 : display the 10 string input*/
+
+int main(void){
+
+
+
+
+
+
+
+    return 0;
+}
